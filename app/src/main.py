c31ae9f667f8586739e21e0a1d6b234a5da0f45d
dumbled0re <smu315064@gmail.com>
2023-04-15 00:48:31 +0900

    update: docker-compose.yml

--- app/src/main.py
+++ app/src/main.py
@@ -1,53 +1,6 @@
-import json
-import os
-
-import requests
 from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
-
-# from utils.slack_notify import SlackNotify
-
-
-class SlackNotify:
-    """
-    スラック通知のクラス
-    """
-
-    def __init__(self) -> None:
-        self.slack_hook_url = os.getenv("SLACK_WEBHOOK_URL")
-        assert self.slack_hook_url is not None, "環境変数にSLACK_WEBHOOK_URLが設定されていません。"
-
-    def slack_notify(
-        self,
-        text: str,
-        username: str = "incoming-webhook",
-        color: str = "good",
-        icon_emoji: str = ":ghost:",
-    ) -> None:
-        """スラックに通知を送るメソッド
-
-        Args:
-            text (str): 送る文字列
-            username (str): ユーザー名
-            color (str): 色
-            icon_emoji (str): アイコンの絵文字
-        """
-
-        data = json.dumps(
-            {
-                "username": username,
-                "icon_emoji": icon_emoji,  # 投稿のプロフィール画像に入れる絵文字
-                "attachments": [
-                    {
-                        "text": text,
-                        "color": color,  # good, warning, danger
-                    }
-                ],
-            }
-        )
-
-        requests.post(self.slack_hook_url, data=data)
-
+from utils.slack_notify import SlackNotify
 
 slack = SlackNotify()
 
