da3b45c8f7ef30150e6da55bf349f0923af2f7c6
dumbled0re <smu315064@gmail.com>
2023-04-17 18:02:51 +0900

    update: Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,23 @@
-FROM --platform=linux/x86_64 python:3.9.16-bullseye
+FROM --platform=linux/amd64 python:3.9.16-bullseye
 
-ENV PYTHONIOENCODING utf-8
+RUN apt-get update -yqq \
+    && apt-get install -yqq \
+        wget \
+        gnupg \
+        unzip \
+    && wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add - \
+    && echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list \
+    && apt-get update -yqq \
+    # 公式リポジトリにあるバージョン確認する
+    # && apt-cache madison google-chrome-stable \
+    && apt-get install -yqq google-chrome-stable=112.0.5615.121-1 \
+    && rm /etc/apt/sources.list.d/google-chrome.list \
+    && rm -rf /var/lib/apt/lists/* \
+    && wget -O /tmp/chromedriver.zip https://chromedriver.storage.googleapis.com/112.0.5615.49/chromedriver_linux64.zip \
+    && unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
 
+# Pythonの依存関係のインストール
 WORKDIR /usr/src/app
 
 COPY requirements.txt ./
 RUN pip install --upgrade pip && pip install --no-cache-dir -r ./requirements.txt
-
-COPY ./app ./
--- app/src/main.py
+++ app/src/main.py
@@ -1,43 +1,18 @@
 from selenium import webdriver
-from selenium.webdriver import DesiredCapabilities
-
-# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
 from utils.slack_notify import SlackNotify
 
+
+def driver_init():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome("/usr/local/bin/chromedriver", options=options)
+    return driver
+
+
 slack = SlackNotify()
-
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--lang=ja-JP")
-chrome_options.add_argument("--disable-dev-shm-usage")
-
-capabilities = DesiredCapabilities.CHROME.copy()
-
-driver = webdriver.Remote(
-    # command_executor="http://selenium:4444/wd/hub",
-    desired_capabilities=capabilities,
-    options=chrome_options,
-)
-
+driver = driver_init()
 driver.get("https://maasaablog.com/")
 slack.slack_notify(driver.title, "apple")
 driver.quit()
-
-
-# from get_chrome_driver import GetChromeDriver
-# from selenium import webdriver
-
-# get_driver = GetChromeDriver()
-# get_driver.install()
-
-
-# def driver_init():
-#     options = webdriver.ChromeOptions()
-#     options.add_argument("--headless")
-#     return webdriver.Chrome(options=options)
-
-
-# driver = driver_init()
-# driver.get("https://maasaablog.com/")
-# slack.slack_notify(driver.title, "apple")
-# driver.quit()
