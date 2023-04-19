06c3d6379c5b65ae98ecc64719e19c5c75d524d3
dumbled0re <smu315064@gmail.com>
2023-04-19 17:25:22 +0900

    update: github-actions.yml

--- src/auto_deposit_rakuten_horse_racing/main.py
+++ src/auto_deposit_rakuten_horse_racing/main.py
@@ -1,20 +1,14 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-# from utils.slack_notify import SlackNotify
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+driver = webdriver.Chrome("/usr/local/bin/chromedriver", options=options)
 
-
-def driver_init():
-    options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome("/usr/local/bin/chromedriver", options=options)
-
-    return driver
-
-
-# slack = SlackNotify()
-driver = driver_init()
-driver.get("https://maasaablog.com/")
-# slack.slack_notify(driver.title, "apple")
+driver.get("https://zenn.dev/")
+time.sleep(3)
+driver.close()
 driver.quit()
